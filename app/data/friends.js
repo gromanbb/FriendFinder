f1728e25bcc9b72bcd786c5213d2b3ab75989f46
gromanbb <54219557+gromanbb@users.noreply.github.com>
2019-10-13 18:35:20 -0500

    Uploaded files with basic logic, structure and dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends info.
+// ===============================================================================
+const friendsArray = [
+  {
+    "name": "Name_1",
+    "photo": "HTTPS_1.jpg",
+    "scores": [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    "name": "Name_2",
+    "photo": "HTTPS_2.jpg",
+    "scores": [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    "name": "Name_3",
+    "photo": "HTTPS_3.jpg",
+    "scores": [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Name_4",
+    "photo": "HTTPS_4.jpg",
+    "scores": [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    "name": "Name_5",
+    "photo": "HTTPS_5.jpg",
+    "scores": [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  }
+];
+
+// Export array to make it accessible to other files using require.
+module.exports = friendsArray;
+  
---
+++ server.js
@@ -0,0 +1,35 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages to give our server useful functionality
+// ==============================================================================
+const express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// Set up the basic properties for our express server
+// ==============================================================================
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Set the port of our server
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTES
+// Point our server to a series of "route" files.
+// ================================================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// Start our server so that it can begin listening to client requests
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
