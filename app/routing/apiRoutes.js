6fcddab5ab7c34ef76c5d3c69965b11a925e74a7
gromanbb <54219557+gromanbb@users.noreply.github.com>
2019-10-13 19:31:00 -0500

    Modified the routes files to set up the GET and POST methods to provide HTML files and capture user's input
    Completed HTML files with formatted layout for Home and Survey pages
    
    Modified survey.html to display the 10 questions, allow user to input data/select answers
    
    Modified survey.html to display match using modal pop-up and to post user's data to server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
 // ===============================================================================
 // LOAD DATA
+// Link our route to a "data" source.
+// The data source holds arrays of information on friends.
 // ===============================================================================
 const friendsData = require("../data/friends");
 
@@ -7,12 +9,24 @@ const friendsData = require("../data/friends");
 // ROUTING
 // ===============================================================================
 module.exports = function(app) {
-
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link they are shown a JSON of the data
+  // ---------------------------------------------------------------------------
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // Then the server saves the data to the friendsData array)
+  // ---------------------------------------------------------------------------
   app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
       friendsData.push(req.body);
       res.json(true);
   });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,6 @@
 // ==============================================================================
 // DEPENDENCIES
+// Series of npm packages to give our server useful functionality
 // ==============================================================================
 const path = require("path");
 
@@ -7,7 +8,10 @@ const path = require("path");
 // ROUTING
 // ===============================================================================
 module.exports = function(app) {
-
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
